18221097fe50b79ef5ea4cec1a815a81c81a32eb
Timothy Zorn <tim@zornco.com>
2017-03-17 17:48:38 -0500

    fix: compile on prepublish when both src and spec exist

--- prepublish.sh
+++ prepublish.sh
@@ -1,11 +1,4 @@
-get_json_val() {
-    python -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)$1))";
-}
-get_npm_command() {
-    local temp=$(echo $npm_config_argv | get_json_val "['original'][0]")
-    echo "$temp" | tr -dc "[:alnum:]"
-}
-if [ $(get_npm_command) != "publish" ]; then
+if [ ! -d "src" ] || [ ! -d "spec" ]; then
     echo "Skipping prepublish script"
     exit 0
 fi
